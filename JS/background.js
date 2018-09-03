474f126e172af0dc206b2c2f575eccee5a1af82c
Ruben Armendariz <rubenarmendarizcarretero@gmail.com>
2018-09-03 17:22:44 +0200

    pista arreglada

--- JS/background.js
+++ JS/background.js
@@ -6,6 +6,10 @@ function Background(game) {
     this.img.src = 'images/road.png';
     this.garden = new Image();
     this.garden.src = 'images/cesped.png';
+    this.img2 = new Image();
+    this.img2.src = 'images/road.png';
+    this.garden2 = new Image();
+    this.garden2.src = 'images/cesped.png';
     this.grada = new Image();
     this.grada.src= 'images/grada.png';
     //this.x = 0;
@@ -15,9 +19,11 @@ function Background(game) {
 Background.prototype.draw = function () {
     this.img.onload = function () {
         for(i=0; i<13 ;i++){
-            this.game.ctx.drawImage(this.grada, 0+i*80, 220, 80, 80);
-            this.game.ctx.drawImage(this.garden, 0+i*80, 300, 80, 150);
-            this.game.ctx.drawImage(this.img, 0+i*80, 450, 80, 150);
+            this.game.ctx.drawImage(this.grada,     0+i*80, 140, 80, 70);
+            this.game.ctx.drawImage(this.garden2,    0+i*80, 210, 80, 30);
+            this.game.ctx.drawImage(this.img2,      0+i*80, 240, 80, 120);
+            this.game.ctx.drawImage(this.garden,    0+i*80, 360, 80, 120);
+            this.game.ctx.drawImage(this.img,       0+i*80, 480, 80, 120);
             
             
             
